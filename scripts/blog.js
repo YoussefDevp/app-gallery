072edb8a9c4a1c26e1c95416af1879e5bfa889e8
youssefDevp <youssefmed212@gmail.com>
2024-04-17 18:10:21 +0200

    app v1

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,7 +3,7 @@ import { creaMenu, creaFooter } from "./compnents.js";
 creaMenu()
 creaFooter(document.querySelector('footer'))
 
-const blog = "http://localhost:3000/blog";
+const blog = "https://stellular-kringle-54ef61.netlify.app/.netlify/functions/api/blog";
 const main = document.querySelector("#main");
 const title = document.querySelector("#title");
 
